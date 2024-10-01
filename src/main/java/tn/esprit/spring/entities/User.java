9ffaaa48817dbb125c6a8f044b285985481606bc
oussamabousbih <oussama.bousbih@esprit.tn>
2024-10-01 14:55:14 +0200

    ajout oussema commentaire

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -18,7 +18,7 @@ import javax.persistence.TemporalType;
 @Table(name = "T_USER")
 public class User implements Serializable {
 
-
+//oussama commentaire
 	private static final long serialVersionUID = 1L;
 
 	@Id
